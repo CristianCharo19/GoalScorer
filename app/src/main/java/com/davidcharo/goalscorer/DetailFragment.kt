fecf7f95f18e8409a26064db84fe7bd34cc6db71
Cristian David Charo <cristian.charo@udea.edu.co>
2021-10-03 15:56:11 -0500

    feature/web_servicies_app se modifica el fragment_detail

--- app/src/main/java/com/davidcharo/goalscorer/DetailFragment.kt
+++ app/src/main/java/com/davidcharo/goalscorer/DetailFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import com.davidcharo.goalscorer.databinding.FragmentDetailBinding
+import com.davidcharo.goalscorer.model.Response
 import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
 
 
 class DetailFragment : Fragment() {
@@ -31,15 +33,31 @@ class DetailFragment : Fragment() {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        /*with(binding) {
+        with(binding) {
             val response = args.response
-            homeTeamTextView.text =
-            descriptionTextView.text = event.strDescriptionEN
-            if (event.strThumb != null) {
-                Picasso.get().load(event.strThumb).into(imageView3)
+            homeTeamTextView.text = response.teams?.home?.name
+            scoreTextView.text = "${response.goals?.home} - ${response.goals?.away}"
+            awayTeamTextView.text = response.teams?.away?.name
+            dateTextView.text = formattedDate(response)
+            nameLeagueTextView.text = "${response.league?.name} ${response.league?.round}"
+            nameRefereeTextView.text = response.fixture?.referee
+            nameVenueTextView.text = response.fixture?.venue?.name
+
+            if (response.league?.logo != null) {
+                Picasso.get().load(response.teams?.home?.logo).into(pictureHomeTeamImageView)
+                Picasso.get().load(response.teams?.away?.logo).into(pictureAwayTeamImageView)
+                Picasso.get().load(response.league?.logo).into(pictureLeagueImageView)
             }
-        }*/
+        }
         return root
+    }
+
+    private fun formattedDate(response: Response): String {
+        val formato = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss+00:00")
+        val formato2 = SimpleDateFormat("EEE d 'de' MMM yy")
+        val fecha = formato.parse(response.fixture?.date)
+        val formattedDate = formato2.format(fecha)
+        return formattedDate
     }
 
 }
